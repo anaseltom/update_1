ffb322eff2f302e65ccd66f131cf41c9127dcc8d
anas <anas-el-tom@live.com>
2024-01-30 21:42:51 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  const currentDate = new Date();
+
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <h2>The time now is {currentDate?.toLocaleTimeString()} </h2>
+    </div>
+  );
+}
+
+export default App;
